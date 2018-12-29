ae337dd93a44c0cf2b5b6e54b7f2185dc50c38f6
JustKuono <46217876+JustKuono@users.noreply.github.com>
2018-12-29 03:57:26 -0600

    Add files via upload

---
+++ meme.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+    let msg = await message.channel.send("Generating memes now...")
+
+    let{body} = await superagent
+    .get(`https://api-to.get-a.life/meme`)
+
+     let mEmbed = new Discord.RichEmbed()
+     .addField("Memes", "It's time for your favorite kind of autism.")
+     .setImage(body.url)
+
+     message.channel.send(mEmbed);
+
+     msg.delete();
+
+}
+
+module.exports.help = {
+
+    name: "meme"
+
+}
