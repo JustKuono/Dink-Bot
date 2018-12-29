6a08b137f15c642d96744740a57c72e6c3a5a382
JustKuono <46217876+JustKuono@users.noreply.github.com>
2018-12-29 04:04:56 -0600

    Update help.js

--- help.js
+++ help.js
@@ -9,11 +9,12 @@ module.exports.run = async (bot, message, args) => {
     .addField("Prefix", "d!")
     .addField("Server Information", "d!serverinfo")
     .addField("Bot Information", "d!botinfo")
+    .addField("Fun Commands", "d!meme for randomly generated autism")
     .addField("Temporary Mute", "d!tempmute @<user> 1s/m/h/d <reason>")
     .addField("Kick", "d!kick @<user> <reason>")
     .addField("Ban", "d!ban @<user> <reason>")
     .addField("Why are reasons needed?", "This allows those who have been kicked/banned to recieve a message in their DMs stating why they were kicked/banned. It can make it easier to discuss appeals as well. ^-^")
-    .addField("Version", "Dink Bot is currently running version 1.0.1")
+    .addField("Version", "Dink Bot is currently running version 1.0.3")
     .addField("New Commands", "At the moment only moderation commands are available until the bot is for sure stable, but if you want to request commands message JustKuono#8763 and I'll look into them and begin writing them ^-^")
 
     return message.channel.send(serverembed);
